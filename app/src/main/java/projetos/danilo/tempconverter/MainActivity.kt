c0eb8b90d991f51911409824f374eb0dad18287a
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-12-07 08:09:50 -0300

    Lógica de conversão das temperaturas

--- app/src/main/java/projetos/danilo/tempconverter/MainActivity.kt
+++ app/src/main/java/projetos/danilo/tempconverter/MainActivity.kt
@@ -2,11 +2,27 @@ package projetos.danilo.tempconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_converter.setOnClickListener { converter(it) }
+    }
+
+    fun converter(view: View){
+        var temp: Double = edt_valorTemp.toString().toDouble()
+
+        if(rd_celcius.isChecked){
+            temp = (temp - 32 * 5/9)
+        } else if (rd_fahreiheint.isChecked) {
+            temp = temp * 9/5 + 32
+        }
+
+        edt_valorTemp.setText(temp.toString())
     }
 }
